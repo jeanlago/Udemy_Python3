0c1a21ddfb08546cb72fa78014ff06014d3fadb3
Jean Lago <100036946+jeanlago@users.noreply.github.com>
2022-03-11 19:38:44 -0300

    Exercícios

--- Exercícios Python Seção04/ex006_conversao_temp.py
+++ Exercícios Python Seção04/ex006_conversao_temp.py
@@ -2,4 +2,4 @@
 
 CELSIUS = float(input("Digite a temperatura em °Celsius: "))
 FAHRENHEIT = CELSIUS * (9.0/5.0)+32
-print(f"a temperatura em farenheint é de: {FAHRENHEIT}° ")
+print(f"a temperatura em farenheint é de: {FAHRENHEIT}°F ")
---
+++ Exercícios Python Seção04/ex007_conversao_temp2.py
@@ -0,0 +1,5 @@
+'''ex007'''
+
+F = float(input("Digite a temperatura em °F: "))
+C = 5.0 * (F -32)/9.0
+print(f"A temperatura em °Celsius é: {C}°C")

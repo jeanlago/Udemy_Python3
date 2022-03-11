2399a2e569d9cefe3f958c56f25bd94f0fc05ba4
Jean Lago <100036946+jeanlago@users.noreply.github.com>
2022-03-11 19:44:05 -0300

    Exercícios

---
+++ Exercícios Python Seção04/ex008_conversao_temp3.py
@@ -0,0 +1,5 @@
+'''ex008'''
+
+K = float(input("Digite a temperatura em °K: "))
+C = K - 273.15
+print(f"A temperatura em °C é: {C}°C")
---
+++ Exercícios Python Seção04/ex009_conversao_temp4.py
@@ -0,0 +1,5 @@
+'''ex009_conversao_temp4'''
+
+C = int(input("Digite a temperatura em °C: "))
+K = C + 273.15
+print(f"A temperatura em Kelvin é: {K}°K")

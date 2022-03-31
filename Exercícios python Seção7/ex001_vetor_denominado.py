8efbdff34c35f999da6803c7df1a8f923b0434ec
Jean Lago <100036946+jeanlago@users.noreply.github.com>
2022-03-31 09:50:12 -0300

    Exercícios

---
+++ Exercícios python Seção7/ex001_vetor_denominado.py
@@ -0,0 +1,11 @@
+'''vetor denominado.'''
+
+#letra A
+lista = [1, 0, 5, -2, -5, 7]
+#letra B
+print(sum(lista[:3]))
+#letra C
+lista.insert(4, 100)
+#letra D
+for i in range (7):
+    print(lista[i:i+1])

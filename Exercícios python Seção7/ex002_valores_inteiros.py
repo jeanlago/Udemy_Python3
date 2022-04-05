9de92bb3a4ae9e27f2ee38e045b18daffe707d0d
JeanLago <100036946+jeanlago@users.noreply.github.com>
2022-04-05 14:18:05 -0300

    Exercícios

---
+++ Exercícios python Seção7/ex002_valores_inteiros.py
@@ -0,0 +1,8 @@
+'''lendo 6 valores inteiros e mostrando os valores'''
+
+VALORES = []
+for i in range (6):
+    X = input('digite um valor: ')
+    VALORES.append(X)
+print(type(VALORES))
+print(VALORES)

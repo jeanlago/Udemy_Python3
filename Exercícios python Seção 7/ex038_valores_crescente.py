e55157e0608c1781ab8ab09f3485386704b0462c
JeanLago <100036946+jeanlago@users.noreply.github.com>
2022-04-28 09:41:39 -0300

    Exercícios.

---
+++ Exercícios python Seção 7/ex038_valores_crescente.py
@@ -0,0 +1,15 @@
+'''guardando valores crescentes em um vetor e ordenando-os.'''
+
+lista = []
+for i in range(10):
+    x = int(input(f'Por favor, digite o {i+1}º valor:'))
+    lista.append(x)
+    lista.sort()
+
+print(lista)
+
+
+
+
+
+
---
+++ Exercícios python Seção 7/ex039_triangulo_pascal.py
@@ -0,0 +1,13 @@
+'''lendo inteiros positivos e printando linhas em triangulo pascal'''
+
+from math import factorial
+n = int(input('Digite um número: '))
+for i in range(n):
+    for j in range(n-i+1):
+        print(end=" ")
+
+    for j in range(i+1):
+        print(factorial(i)//(factorial(j)*factorial(i-j)), end=" ")
+
+
+    print()

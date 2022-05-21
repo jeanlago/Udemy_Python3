8ad9ea2d85360fb2b3b6116ab3fca8841662584e
JeanLago <100036946+jeanlago@users.noreply.github.com>
2022-05-21 06:48:19 -0300

    Fix

--- Exercícios Python Seção 8/ex033_soma_algarismos.py
+++ Exercícios Python Seção 8/ex033_soma_algarismos.py
@@ -6,6 +6,8 @@ def soma_algarismos(NUM):
 valor = int(input("Digite o valor: "))
 
 print(f'A divisão do valor {valor} em algarismos fica:',end=' ')
+#O operador * em Python pode ser usado para descompactar objetos.
+# Ele descompacta todos os elementos de uma lista e os imprime sem os colchetes, conforme mostrado abaixo.
 print(*soma_algarismos(valor), sep=' + ',end=' = ')
 print(sum(soma_algarismos(valor)))
 
